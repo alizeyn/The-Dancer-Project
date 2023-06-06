18211580b919b3f3564c0657ec6c39bc7b27ba6c
Ali Zeynali <the.ali.zeynali@gmail.com>
2023-06-06 21:28:18 +0200

    change background and fractile color based on the spectral centroid

--- fractalTreeDancer.js
+++ fractalTreeDancer.js
@@ -1,5 +1,7 @@
 let mic;
 let fft;
+let bgColor;
+let lineColor;
 
 function setup() {
   createCanvas(800, 800);
@@ -8,63 +10,66 @@ function setup() {
   fft = new p5.FFT();
   fft.setInput(mic);
   angleMode(DEGREES);
+  bgColor = color(64, 224, 208);
+  lineColor = color(255);
 }
 
 function draw() {
-  background(64, 224, 208); // set the background to turquoise
   let vol = mic.getLevel();
-
-  // Calculate the base branch length based on the sound volume
   let baseBranchLen = map(vol, 0, 1, 20, 200) * 5;
-
-  // Calculate the branch angle based on the centroid of the frequency spectrum
   let spectrum = fft.analyze();
   let centroid = fft.getCentroid();
-  let angle = map(centroid, 0, width / 2, 0, 90);
 
-  // Draw seven trees equally spaced around the center of the canvas
-  translate(width / 2, height / 2);
-  stroke(255); // set the line color to white
-  strokeWeight(2); // increase the line thickness by one level
-
-  for(let i = 0; i < 7; i++) {
-    push(); // Save the current state of transformation
-    rotate(i * 360 / 7); // Rotate to draw each tree in a different direction
-    branch(baseBranchLen, angle); // Draw the tree
-    pop(); // Restore the state of transformation
+  if(centroid < 2000) { // Increased from 1000 to 2000
+    bgColor = color(0, 71, 171); // Cobalt blue
+  } else {
+    bgColor = color(64, 224, 208); // Turquoise
   }
 
-  // Draw seven smaller trees in the center
+  if(centroid > 3000) { // Decreased from 5000 to 3000
+    lineColor = color(255, 0, 0); // Solid red
+  } else {
+    lineColor = color(255); // White
+  }
+
+  background(bgColor);
+  stroke(lineColor);
+  strokeWeight(2);
+
+  let angle = map(centroid, 0, width / 2, 0, 90);
+
+  translate(width / 2, height / 2);
+
   for(let i = 0; i < 7; i++) {
-    push(); // Save the current state of transformation
-    rotate(i * 360 / 7); // Rotate to draw each tree in a different direction
-    branch(baseBranchLen * 0.25, angle); // Draw the tree
-    pop(); // Restore the state of transformation
+    push();
+    rotate(i * 360 / 7);
+    branch(baseBranchLen, angle);
+    pop();
+  }
+
+  for(let i = 0; i < 7; i++) {
+    push();
+    rotate(i * 360 / 7);
+    branch(baseBranchLen * 0.25, angle);
+    pop();
   }
 }
 
 function branch(len, angle) {
-  // Draw the branch
   line(0, 0, 0, -len);
-
-  if(len < 2) { // decrease this value to allow more levels of recursion
-    // Base case: if the branch is too short, don't draw any more branches
+  if(len < 2) {
     return;
   } else {
-    // Recursive case: draw two smaller branches at the end of this one
+    push();
+    translate(0, -len);
+    rotate(angle);
+    branch(len * 0.67, angle);
+    pop();
 
-    // First branch
-    push(); // Save the current state of transformation
-    translate(0, -len); // Move to the end of the branch
-    rotate(angle); // Rotate by a certain angle
-    branch(len * 0.67, angle); // Draw a smaller branch
-    pop(); // Restore the state of transformation
-
-    // Second branch
-    push(); // Save the current state of transformation
-    translate(0, -len); // Move to the end of the branch
-    rotate(-angle); // Rotate by a negative angle
-    branch(len * 0.67, angle); // Draw a smaller branch
-    pop(); // Restore the state of transformation
+    push();
+    translate(0, -len);
+    rotate(-angle);
+    branch(len * 0.67, angle);
+    pop();
   }
 }
